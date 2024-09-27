9ea9a81ab117003c888724457d6f6d46388ad066
Rushabh Modi <rushabh@atharvasystem.com>
2024-09-27 18:18:47 +0530

    setup and home route

--- app/components/example_component.rb
+++ app/components/example_component.rb
@@ -4,5 +4,7 @@ class ExampleComponent < ViewComponent::Base
   def initialize(title:)
     @title = title
   end
-
+  # erb_template <<-ERB
+  #   <span title="<%= @title %>"><%= content %></span>
+  # ERB
 end
